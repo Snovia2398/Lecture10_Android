23cc97b58a622b1f1cb4e2a99c876e7d461a979f
Snovia2398 <snovia2398@gmail.com>
2021-03-31 16:51:36 +0500

    With db

--- Lecture10/app/src/main/java/com/snovia/sqlite/MainActivity.java
+++ Lecture10/app/src/main/java/com/snovia/sqlite/MainActivity.java
@@ -32,8 +32,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                try {
                     customerModel = new CustomerModel(editName.getText().toString(), Integer.parseInt(editAge.getText().toString()), switchIsActive.isChecked(), 1);
                     Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                boolean b = dbHelper.addCustomer(customerModel);
+
             }
         });
         buttonViewAll.setOnClickListener(new View.OnClickListener() {
