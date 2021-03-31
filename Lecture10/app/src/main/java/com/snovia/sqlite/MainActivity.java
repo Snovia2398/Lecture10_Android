8d08b8ecb64fc215da68be8d383091f24f5772d7
Snovia2398 <snovia2398@gmail.com>
2021-03-31 16:12:37 +0500

    Sqlite without db

---
+++ Lecture10/app/src/main/java/com/snovia/sqlite/MainActivity.java
@@ -0,0 +1,48 @@
+package com.snovia.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonAdd,buttonViewAll;
+    EditText editName,editAge;
+    Switch switchIsActive;
+    ListView listViewDetails;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonViewAll = findViewById(R.id.buttonViewAll);
+        editName = findViewById(R.id.editTextName);
+        editAge = findViewById(R.id.editTextAge);
+        switchIsActive = findViewById(R.id.switchIsActive);
+        listViewDetails = findViewById(R.id.listViewDetails);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            CustomerModel customerModel;
+
+            @Override
+            public void onClick(View v) {
+                    customerModel = new CustomerModel(editName.getText().toString(), Integer.parseInt(editAge.getText().toString()), switchIsActive.isChecked(), 1);
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        buttonViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "View all clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
